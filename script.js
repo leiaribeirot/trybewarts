8873e5df591dbd8c62b7f46deed8f297a5b12e6f
Léia Ribeiro <leiaribeirot@gmail.com>
2021-12-07 16:47:35 -0400

    Dev: HTML -  ajustes - Requisitos de 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const botaoLogin = document.querySelector('#botaoLogin');
 const login = document.querySelector('#login');
 const password = document.querySelector('#password');
+const agree = document.querySelector('#agreement');
+const submitBtn = document.querySelector('#submit-btn');
 
 // Validação Login
 
@@ -9,5 +11,19 @@ botaoLogin.addEventListener('click', () => {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+// Checa do Botão Agree
+
+window.onload = () => {
+  submitBtn.disabled = true;
+};
+
+agree.addEventListener('click', () => {
+  if (agree.checked === false) {
+    submitBtn.disabled = true;
+  } else {
+    submitBtn.disabled = false;
   }
 });
