3c12f1e05e849795fcac231739aef0851a92b861
Ques-Mendes <quesmendes@gmail.com>
2021-12-08 14:47:51 -0300

    Requisito 21 - parcial

--- script.js
+++ script.js
@@ -36,3 +36,35 @@ textArea.addEventListener('keyup', () => {
   const cont = textArea.value.length;
   contador.innerText = areaText - cont;
 });
+
+const materias = document.querySelectorAll('.subject');
+function checkMaterias() {
+  const materiasChecked = [];
+  for (let index = 0; index < materias.length; index += 1) {
+    if (materias[index].checked) {
+      materiasChecked.push(`${materias[index].value}`);
+    }
+  }
+  return materiasChecked;
+}
+
+function validate() {
+  submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('input-name').value;
+    const lastName = document.getElementById('input-lastname').value;
+    const emailInput = document.getElementById('input-email').value;
+    const house = document.getElementById('house').value;
+    const family = document.querySelector('input[name="family"]:checked').value;
+    const avaliacao = document.querySelector('input[name ="rate"]:checked').value;
+    const forms = document.getElementById('evaluation-form');
+    const observacoes = document.getElementById('textarea').value;
+    forms.innerHTML = '';
+    const formulario = `Nome: ${name} ${lastName} Email: ${emailInput} 
+    Casa: ${house} Família: ${family} Matérias: ${checkMaterias()}
+    Avaliação: ${avaliacao}
+    Observações: ${observacoes}`;
+    forms.innerText = formulario;
+  });
+}
+validate();
